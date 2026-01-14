03768447b82a25168b0e792afbbb46bdb3410420
vivek shekhawat <vshekhawat136@gmail.com>
2026-01-14 11:52:52 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import ContentBoxMainPage from "./contentBoxMainPage"
+import TopNavBar from "./TopNavBar"
+
+function Home() {
+  return (
+    <div id="homePage">
+        <TopNavBar/>
+        <ContentBoxMainPage/>
+        
+    </div>
+  )
+}
+
+export default Home
